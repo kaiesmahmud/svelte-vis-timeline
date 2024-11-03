38e8fc598cda137d5161d3830ae8019217742d36
kaiesmahmud <kaiesmahmud@gmail.com>
2024-11-03 21:13:06 +0600

    drag and drop moved to  page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
     import "../app.css";
   </script>
-  
+  <div class="flex items-center justify-center flex-col w-full">
+    <p>Pages</p>
+    <div class="flex flex-wrap items-center justify-center">
+      <a href="./drag-and-drop">1. Timeline Drag & Drop Example</a>
+    </div>
+  </div>
   <slot />
+  <style>
+    a{
+      @apply p-5 underline text-slate-800 hover:text-slate-700  italic font-semibold ;
+    }
+  </style>
