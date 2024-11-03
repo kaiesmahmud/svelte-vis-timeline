e2afdce5a7583543b67b0b17841e3e37e4a36c2c
kaiesmahmud <kaiesmahmud@gmail.com>
2024-11-03 22:41:13 +0600

    some links,notes and css  add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script>
-    import "../app.css";
-  </script>
-  <div class="flex items-center justify-center flex-col w-full">
-    <p>Pages</p>
-    <div class="flex flex-wrap items-center justify-center">
+  import "../app.css";
+</script>
+<div class="w-full flex items-center justify-center p-5 ">
+  <div class="bg-white w-auto  flex items-center justify-center flex-col rounded-lg p-5">
+    <p class="text-xl">svelte-vis-timeline</p>
+    <div class="flex flex-wrap items-center justify-center gap-3">
       <a href="./drag-and-drop">1. Timeline Drag & Drop Example</a>
+      <a href="./update-data-on-event">2. Timeline update data on event Example</a>
     </div>
-  </div>
-  <slot />
-  <style>
-    a{
-      @apply p-5 underline text-slate-800 hover:text-slate-700  italic font-semibold ;
-    }
-  </style>
+</div>
+</div>
+<slot />
+
+<style>
+  a {
+    @apply p-3 underline text-slate-800 hover:text-slate-700  italic font-semibold;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,47 @@
 <script>
-	
 </script>
-<main class="w-full h-full flex items-center justify-center flex-col">
 
-</main>
+<div class="flex items-center justify-center">
+    <main class="bg-white border rounded-lg h-full flex flex-col gap-5 p-10">
+        <p>
+            Nested Tree folder: 
+            <a
+                href="https://visjs.github.io/vis-timeline/examples/timeline/groups/nestedThreeLevels.html"
+                >https://visjs.github.io/vis-timeline/examples/timeline/groups/nestedThreeLevels.html</a
+            >
+        </p>
+        <p>
+            Moving timebar play head:
+            <a
+                href="https://visjs.github.io/vis-timeline/examples/timeline/editing/updateDataOnEvent.html"
+                >https://visjs.github.io/vis-timeline/examples/timeline/editing/updateDataOnEvent.html</a
+            >
+        </p>
+        <p>
+            Stacking or No Stacking of subgroups:
+            <a
+                href="https://visjs.github.io/vis-timeline/examples/timeline/groups/subgroups.html"
+                >https://visjs.github.io/vis-timeline/examples/timeline/groups/subgroups.html</a
+            >
+        </p>
+        <p>
+            Serialization:
+            <a
+                href="https://visjs.github.io/vis-timeline/examples/timeline/dataHandling/dataSerialization.html"
+                >https://visjs.github.io/vis-timeline/examples/timeline/dataHandling/dataSerialization.html
+            </a>
+        </p>
+        <p>
+            VISJS ecosystem:
+            <a href="https://github.com/visjs/awesome-visjs"
+                >https://github.com/visjs/awesome-visjs</a
+            >
+        </p>
+    </main>
+</div>
 
-
+<style>
+    a {
+        @apply hover:underline p-5;
+    }
+</style>
