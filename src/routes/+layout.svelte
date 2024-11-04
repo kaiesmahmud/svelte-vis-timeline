dfaecaf6534371e67d81805e504ab1dc77ad780b
kaiesmahmud <kaiesmahmud@gmail.com>
2024-11-04 21:18:26 +0600

    home link added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 <div class="w-full flex items-center justify-center p-5 ">
   <div class="bg-white w-auto  flex items-center justify-center flex-col rounded-lg p-5">
-    <p class="text-xl">svelte-vis-timeline</p>
+    <p class="text-xl"><a href="./">svelte-vis-timeline</a></p>
     <div class="flex flex-wrap items-center justify-center gap-3">
       <a href="./drag-and-drop">1. Timeline Drag & Drop Example</a>
       <a href="./update-data-on-event">2. Timeline update data on event Example</a>
